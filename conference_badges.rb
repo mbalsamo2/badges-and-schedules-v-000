940e0a8217447ec302c34d610099da80347a74b8
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-19 21:18:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,8 @@ def batch_badge_creator(array)
   array.collect{|name| "Hello, my name is #{name}."}
 end
 
-# def batch_badge_creator(array)
-#   speaker_badges = []
-#   array.each{|name| speaker_badges.push("Hello, my name is #{name}.")}
-#   speaker_badges
-# end
+def assign_rooms(array)
+  counter = 1
+  array.collect{|name| "Hello, #{name}! You'll be assigned to room #{counter}!"}
+  counter += 1
+end
