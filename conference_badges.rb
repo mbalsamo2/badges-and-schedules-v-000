b1d6d8aad8114a0e40ac9c57d66aa87a4f7a06b9
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 00:13:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array).collect
-  puts assign_rooms(array).collect
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
