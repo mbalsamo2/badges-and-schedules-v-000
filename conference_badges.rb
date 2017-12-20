e4745b3d0bdf432351693d2871e4947803d321fc
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 00:26:35 +0000

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+  array.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(array)
+  batch_badge_creator(array).each {|nametag| puts nametag}
+  assign_rooms(array).each {|rooms| puts rooms}
+end
