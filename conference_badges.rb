7397f287cb19e6790e08f490a674da244e2d975b
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 00:22:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each {|nametag| return "#{nametag}" }
+  batch_badge_creator(array).each {|nametag| puts nametag }
   puts assign_rooms(array)
 end
